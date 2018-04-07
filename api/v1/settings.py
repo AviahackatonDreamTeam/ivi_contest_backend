e578349856a34a894d1478ae28679cc8b1ef1772
Cdayz <cdayz@yandex.ru>
2018-04-08 02:00:57 +0300

    Отредактировал конфиг

--- api/v1/settings.py
+++ api/v1/settings.py
@@ -1,16 +1,17 @@
 from v1.films import films
 from v1.users import users
+from os import environ as env
 
 DOMAIN = {
     'users' : users,
     'films' : films,
 }
 
-MONGO_HOST = 'localhost'
+MONGO_HOST = '0.0.0.0'
 MONGO_PORT = 27017
 
 # Skip these if your db has no auth. But it really should.
-MONGO_USERNAME = '<your username>'
-MONGO_PASSWORD = '<your password>'
+MONGO_USERNAME = 'ApiAdmin'
+MONGO_PASSWORD = env['APP_MONGO_TOKEN']
 
 MONGO_DBNAME = 'apitest'
